28c678f4f7e22cd0b1fe79cd1a5d230f5cde5336
wangfangxiang <1035504910@qq.com>
2019-04-21 13:05:03 +0800

    用户登录注册的sql

--- cdhotel/src/main/java/com/cdhotel/mapper/UserMapper.java
+++ cdhotel/src/main/java/com/cdhotel/mapper/UserMapper.java
@@ -1,5 +1,7 @@
 package com.cdhotel.mapper;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.cdhotel.model.User;
 
 public interface UserMapper {
@@ -14,4 +16,10 @@ public interface UserMapper {
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
+
+	String getUserName(String username);
+
+	void insertUser(@Param("username")String username, @Param("password")String password, @Param("role")String role, @Param("datetime")String datetime);
+
+	User getUser(String username);
 }
